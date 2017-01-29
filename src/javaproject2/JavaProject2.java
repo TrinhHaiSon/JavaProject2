e52cbee45f0eb621424a061fcd48830bc11448d2
trinhhaison <haisontrinh96@gmail.com>
2017-01-29 22:57:42 +0700

    Edit JavaProject2.java from local master

--- src/javaproject2/JavaProject2.java
+++ src/javaproject2/JavaProject2.java
@@ -16,6 +16,7 @@ public class JavaProject2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.print("Hello world");
     }
     
 }
