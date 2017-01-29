8c7507a34150b7959bd5a1bbbeb236782eee2c69
trinhhaison <haisontrinh96@gmail.com>
2017-01-30 01:27:18 +0700

    Recall NewClass1.java on local master

---
+++ src/javaproject2/NewClass1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaproject2;
+
+/**
+ *
+ * @author root
+ */
+public class NewClass1 {
+    private int a;
+    private int b;
+
+    public NewClass1(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+    
+    
+}
